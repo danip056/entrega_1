ab75dc139268741c9b1c3b37f8089385bb913e08
dsp <danip056@gmail.com>
2023-02-23 23:33:16 -0500

    fix: unique email constraint. feat: task-user relationship addded.

--- backend/v1/models.py
+++ backend/v1/models.py
@@ -3,6 +3,9 @@ from sqlalchemy import Column, String, TIMESTAMP, text, ForeignKey
 from sqlalchemy.dialects.postgresql import INTEGER, BOOLEAN
 from sqlalchemy.sql import func
 from sqlalchemy import Enum
+from sqlalchemy.orm import relationship, Mapped
+from typing import List
+
 
 Base = declarative_base()
 metadata = Base.metadata
@@ -11,14 +14,16 @@ class User(Base):
     __tablename__ = 'user'
     id = Column(INTEGER(), primary_key=True)
     user = Column(String(120), unique=True)
-    email = Column(String(120))
+    email = Column(String(120), unique=True)
     password = Column(String(300))
+    tasks: Mapped[List["Task"]] = relationship(back_populates="user")
 
 
 class Task(Base):
     __tablename__ = 'task'
     id = Column(INTEGER(), primary_key=True)
     user_id = Column(INTEGER(), ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="tasks")
     original_file_name = Column(String(300))
     original_file_ext = Column(String(10))
     original_stored_file_name = Column(String(300))
