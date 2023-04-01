cbc283990535a7dd31afc88c421b413edd4090ea
dsp <danip056@gmail.com>
2023-04-01 01:38:46 -0500

    fix: nfs parameters and sh composition script options fixed

--- compose_services_prod.sh
+++ compose_services_prod.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Select the service you want to compose:"
-select env in "All" "Web" "Worker" "File" "BD"; do
+select env in "Web" "Worker"; do
     case $env in
         Web ) docker compose -f "docker-compose-web-prod.yml" up -d; break;;
         Worker ) docker compose -f "docker-compose-worker-prod.yml" up -d; break;;
