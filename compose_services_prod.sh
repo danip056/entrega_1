ecc458ae975a97baaaf5e182a9eaf195f0921f22
dsp <danip056@gmail.com>
2023-05-06 21:37:09 -0500

    fix: compose build sh updated

--- compose_services_prod.sh
+++ compose_services_prod.sh
@@ -3,8 +3,8 @@
 echo "Select the service you want to compose:"
 select env in "Web" "Worker"; do
     case $env in
-        Web ) docker compose -f "docker-compose-web-prod.yml" up -d; break;;
-        Worker ) docker compose -f "docker-compose-worker-prod.yml" up -d; break;;
+        Web ) docker compose -f "docker-compose-web-prod.yml" up -d --build; break;;
+        Worker ) docker compose -f "docker-compose-worker-prod.yml" up -d --build; break;;
         cancel ) echo "No option was selected, exit."; exit;;
         * ) echo "Please select shown options.";;
     esac
